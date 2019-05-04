04f35e6ffb3d885b9e303de8378d5c1aa5fb3474
Douglas <greyling.douglas@gmail.com>
2019-05-04 12:14:11 +0200

    Updated mars rover spec

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,76 +1,67 @@
 require "rspec"
 require "./mars_rover.rb"
+require "./mars_plateau.rb"
 
 describe MarsRover do
-  subject { MarsRover.new(5,6) }
+  subject { MarsRover.new(@plateau) }
 
-  it "should initialize the plateau and directions" do
-    expect(subject.plat_width).to eq 5
-    expect(subject.plat_length).to eq 6
-    expect(subject.directions).to eq MarsRover::DIRECTIONS
+  before(:all) do
+    @plateau = MarsPlateau.new(5,6)
+  end
+
+  it "should initialize the position to nil" do
+    expect(subject.position).to eq({ x: nil, y: nil, dir: nil })
   end
 
   it "should set the correct position with valid input" do
     subject.set_position_and_direction(1,2,'N')
 
-    expect(subject.pos_x).to eq 1
-    expect(subject.pos_y).to eq 2
-    expect(subject.current_dir).to eq 'N'
-    expect(subject.position).to eq "Position: (1, 2, N)."
+    expect(subject.position).to eq({ x: 1, y: 2, dir: 'N' })
   end
 
   it "should set the default position with invalid x/y input" do
     subject.set_position_and_direction(-1,2,'N')
 
-    expect(subject.pos_x).to eq 0
-    expect(subject.pos_y).to eq 0
-    expect(subject.current_dir).to eq 'N'
-    expect(subject.position).to eq "Position: (0, 0, N)."
+    expect(subject.position).to eq({ x: 0, y: 0, dir: 'N' })
 
     subject.set_position_and_direction(1,-2,'N')
 
-    expect(subject.pos_x).to eq 0
-    expect(subject.pos_y).to eq 0
-    expect(subject.current_dir).to eq 'N'
-    expect(subject.position).to eq "Position: (0, 0, N)."
+    expect(subject.position).to eq({ x: 0, y: 0, dir: 'N' })
   end
 
   it "should set the default position with invalid directional input" do
     subject.set_position_and_direction(1,2,'Z')
 
-    expect(subject.pos_x).to eq 0
-    expect(subject.pos_y).to eq 0
-    expect(subject.current_dir).to eq 'N'
-    expect(subject.position).to eq "Position: (0, 0, N)."
+    expect(subject.position).to eq({ x: 0, y: 0, dir: 'N' })
   end
 
   it "should rove to the correct position" do
+    @plateau.grid = []
+
     subject.set_position_and_direction(1,2,'N')
     subject.rove('LMLMLMLMM')
 
-    expect(subject.pos_x).to eq 1
-    expect(subject.pos_y).to eq 3
-    expect(subject.current_dir).to eq 'N'
-    expect(subject.position).to eq "Position: (1, 3, N)."
+    expect(subject.position).to eq({ x: 1, y: 3, dir: 'N' })
+    expect(@plateau.grid).to include subject.xy_position
   end
 
   it "should not rove when instructions are invalid" do
+    @plateau.grid = []
+
     subject.set_position_and_direction(1,2,'N')
     subject.rove('LMLMLMLX')
 
-    expect(subject.pos_x).to eq 1
-    expect(subject.pos_y).to eq 2
-    expect(subject.current_dir).to eq 'N'
-    expect(subject.position).to eq "Position: (1, 2, N)."
+    expect(subject.position).to eq({ x: 1, y: 2, dir: 'N' })
+    expect(@plateau.grid).not_to include subject.xy_position
   end
 
   it "should raise an error when it tries to move out of bounds" do
+    @plateau.grid = []
+
     subject.set_position_and_direction(1,2,'N')
     subject.rove('LMLMLMLMMMMM')
 
-    expect(subject.pos_x).to eq 1
-    expect(subject.pos_y).to eq 6
-    expect(subject.current_dir).to eq 'N'
-    expect(subject.position).to eq "Position: (1, 6, N)."
+    expect(subject.position).to eq({ x: 1, y: 6, dir: 'N' })
+    expect(@plateau.grid).to include subject.xy_position
   end
 end
